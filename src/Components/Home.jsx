65fb97ae7329175c421aecd5b45480f81302242f
Muhammad Gad <106442148+GADMuhammad@users.noreply.github.com>
2024-12-21 21:47:30 +0200

    Add files via upload

---
+++ src/Components/Home.jsx
@@ -0,0 +1,21 @@
+import Best from "./Best";
+import BrowseByCategory from "./BrowseByCategory";
+import Enhance from "./Enhance";
+import Features from "./Features";
+import FlashSale from "./FlashSale";
+import Introduction from "./Introduction";
+import Arrival from "./Components/Arrival/";
+
+export default function Home({ timerDate }) {
+  return (
+    <>
+      <Introduction />
+      <FlashSale timerDate={timerDate} />
+      <BrowseByCategory />
+      <Best all={false} />
+      <Enhance timerDate={timerDate} />
+      <Arrival />
+      <Features />
+    </>
+  );
+}
